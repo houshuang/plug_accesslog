d900acde31af180088284ecd77dcf2768f096b26
Stian Håklev <shaklev@gmail.com>
2015-07-01 17:10:25 +0800

    Specializing on my user id

--- lib/plug/accesslog/formatter/remote_user.ex
+++ lib/plug/accesslog/formatter/remote_user.ex
@@ -11,27 +11,16 @@ defmodule Plug.AccessLog.Formatter.RemoteUser do
   @spec append(String.t, Plug.Conn.t) :: String.t
   def append(message, conn), do: message <> remote_user(conn)
 
-  defp get_user(credentials) do
+  defp remote_user(conn) do
     try do
-      case parse_credentials(credentials) do
-        [ user, _pass ] -> user
-        _               -> "-"
+      if a = conn.private.plug_session["repo_userid"] do
+        "#{a}"
+      else
+        "-"
       end
     rescue
       _ -> "-"
     end
   end
 
-  defp parse_credentials(credentials) do
-    credentials
-    |> Base.decode64!()
-    |> String.split(":")
-  end
-
-  defp remote_user(conn) do
-    case get_req_header(conn, "authorization") do
-      [<< "Basic ", credentials :: binary >>] -> get_user(credentials)
-      _ -> "-"
-    end
-  end
 end
